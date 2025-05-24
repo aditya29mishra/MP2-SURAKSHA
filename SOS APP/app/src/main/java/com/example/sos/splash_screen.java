bd04d609892d7b8a8c2a2886b562da043fd8dd86
aditya29mishra <adityarakeshmishra@gmail.com>
2025-05-24 11:14:50 +0530

    SOS App commit

---
+++ SOS APP/app/src/main/java/com/example/sos/splash_screen.java
@@ -0,0 +1,28 @@
+package com.example.sos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+
+public class splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent I = new Intent(splash_screen.this, HomeActivity.class);
+                startActivity(I);
+                finish();
+            }
+        },1000);
+    }
+}
